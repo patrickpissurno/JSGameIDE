c4e43253e5c70e120c90862f7b4396fdc5d0fd7c
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2016-01-27 15:04:23 -0200

    [Framework] UI Movable Support

--- Source/JSGameIDE/JSGameIDE/Resources/UIs.js
+++ Source/JSGameIDE/JSGameIDE/Resources/UIs.js
@@ -16,6 +16,14 @@
         this.args = args;
         this.onDestroy = null;
         this.result = null;
+        this.movable = {
+            allowed : $UIMovable,
+            offset : {
+                x : 0,
+                y : 0
+            },
+            moving : false
+        };
         
         //Instantiate
         roomManager.actual[this.name].push(this);
@@ -44,33 +52,10 @@
                 this._create_executed = true;
             };
             
-            //Mouse
-            if((!mouse.pressed[0] && this.pressed[0])||
-               (!mouse.pressed[1] && this.pressed[1])||
-               (!mouse.pressed[2] && this.pressed[2]))
-            {
-                var event = {button : (this.pressed[0] && !mouse.pressed[0] ? 0 : this.pressed[1] && !mouse.pressed[1] ? 1 : 2)};
-                this.pressed = [mouse.pressed[0], mouse.pressed[1], mouse.pressed[2]];
-                //$UIMouseReleased
-            };
-            
-            if(((mouse.pressed[0] && !this.pressed[0])||
-               (mouse.pressed[1] && !this.pressed[1])||
-               (mouse.pressed[2] && !this.pressed[2])) && roomManager.actual.camera.x + mouse.x > this.x  && roomManager.actual.camera.x + mouse.x < this.x + this.width && roomManager.actual.camera.y + mouse.y > this.y && roomManager.actual.camera.y + mouse.y < this.y + this.height)
-            {
-                var event = {button : (!this.pressed[0] && mouse.pressed[0] ? 0 : !this.pressed[1] && mouse.pressed[1] ? 1 : 2)};
-                this.pressed = [mouse.pressed[0], mouse.pressed[1], mouse.pressed[2]];
-                //$UIMousePressed
-            };
-            
-            if(this.toDestroy)
-            {
-                this.destroy();
-            };
-            
-            $UIUpdate
-            
             var alignedPos = UI.GetAlignedPosition(this.x, this.y, this.width, this.height, this.align, 1);
+            
+            var mouseHandled = false;
+            //Update the components
             for(var i=0; i<this.components.length; i++){
                 if(this.components[i] != null)
                 {
@@ -85,10 +70,19 @@
                             if((mouse.pressed[0] && !comp.pressed[0])||(mouse.pressed[1] && !comp.pressed[1])||
                                (mouse.pressed[2] && !comp.pressed[2]))
                             {
-                                var event = {button : (!comp.pressed[0] && mouse.pressed[0] ? 0 : !comp.pressed[1] && mouse.pressed[1] ? 1 : 2)};
+                                var event = {
+                                    button : (!comp.pressed[0] && mouse.pressed[0] ? 0 : !comp.pressed[1] && mouse.pressed[1] ? 1 : 2),
+                                    handled : true
+                                };
                                 comp.pressed = [mouse.pressed[0], mouse.pressed[1], mouse.pressed[2]];
                                 if(comp.mousePressed != null)
+                                {
                                     comp.mousePressed(event);
+                                    if(event.handled)
+                                        mouseHandled = true;
+                                }
+                                else if(comp.mouseReleased != null)
+                                    mouseHandled = true;
                             }
                         }
                         
@@ -97,16 +91,71 @@
                           (!mouse.pressed[1] && comp.pressed[1])||
                           (!mouse.pressed[2] && comp.pressed[2]))
                         {
-                            var event = {button : (comp.pressed[0] && !mouse.pressed[0] ? 0 : comp.pressed[1] && !mouse.pressed[1] ? 1 : 2)};
+                            var event = {
+                                button : (comp.pressed[0] && !mouse.pressed[0] ? 0 : comp.pressed[1] && !mouse.pressed[1] ? 1 : 2),
+                                handled : true
+                            };
                             comp.pressed = [mouse.pressed[0], mouse.pressed[1], mouse.pressed[2]];
                             if(comp.mouseReleased != null)
+                            {
                                 comp.mouseReleased(event);
+                                if(event.handled)
+                                    mouseHandled = true;
+                            }
                         }
+                        
+                        if(comp.pressed[0])
+                            mouseHandled = true;
                     }
                     if(comp.update != null)
                         comp.update();
                 }
             }
+                        
+            
+            //Mouse
+            if((!mouse.pressed[0] && this.pressed[0])||
+               (!mouse.pressed[1] && this.pressed[1])||
+               (!mouse.pressed[2] && this.pressed[2]))
+            {
+                var event = {button : (this.pressed[0] && !mouse.pressed[0] ? 0 : this.pressed[1] && !mouse.pressed[1] ? 1 : 2)};
+                this.pressed = [mouse.pressed[0], mouse.pressed[1], mouse.pressed[2]];
+                //$UIMouseReleased
+                if(this.movable.allowed && this.movable.moving)
+                {
+                    this.movable.moving = false;
+                }
+            };
+            
+            if(((mouse.pressed[0] && !this.pressed[0])||
+               (mouse.pressed[1] && !this.pressed[1])||
+               (mouse.pressed[2] && !this.pressed[2])) && mouse.x > alignedPos.x  && mouse.x < alignedPos.x + alignedPos.width && mouse.y > alignedPos.y && mouse.y < alignedPos.y + alignedPos.height && !mouseHandled)
+            {
+                var event = {button : (!this.pressed[0] && mouse.pressed[0] ? 0 : !this.pressed[1] && mouse.pressed[1] ? 1 : 2)};
+                this.pressed = [mouse.pressed[0], mouse.pressed[1], mouse.pressed[2]];
+                //$UIMousePressed
+                if(this.movable.allowed && !this.movable.moving)
+                {
+                    this.movable.offset.x = alignedPos.x - mouse.x;
+                    this.movable.offset.y = alignedPos.y - mouse.y;
+                    this.movable.moving = true;
+                }
+            };
+            
+            if(this.toDestroy)
+            {
+                this.destroy();
+            };
+            
+            if(this.pressed[0] && this.movable.allowed && this.movable.moving)
+            {
+                var offsetX = alignedPos.x - this.x;
+                var offsetY = alignedPos.y - this.y;
+                this.x = mouse.x - offsetX + this.movable.offset.x;
+                this.y = mouse.y - offsetY + this.movable.offset.y;
+            }            
+            
+            $UIUpdate
         };
         
         //UI Draw Event
